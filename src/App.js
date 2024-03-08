40da33bcb13297bc5d6c277b53e9c2572834d6d5
nvdinh185 <nvdinh0766777123@gmail.com>
2024-03-08 08:03:19 +0700

    bai1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,143 @@
 export default function App() {
   return (
     <div>
-      <h1>Hello World!</h1>
+      <a href="#" title="header.jpg">
+        <img src="/images/header.jpg" alt="header.jpg" />
+      </a>
+      <ul>
+        <li>
+          <a href="#" title="Trang chủ">
+            Trang chủ
+          </a>
+        </li>
+        <li>
+          <a href="#" title="Tin tức">
+            Tin tức
+          </a>
+        </li>
+      </ul>
+      <div>
+        <h3>Danh mục tin</h3>
+        <ul>
+          <li>
+            <a href="#" title="Thời sự">
+              Thời sự
+            </a>
+          </li>
+          <li>
+            <a href="#" title="Góc nhìn">
+              Góc nhìn
+            </a>
+          </li>
+          <li>
+            <a href="#" title="Thế giới">
+              Thế giới
+            </a>
+          </li>
+          <li>
+            <a href="#" title="Kinh doanh">
+              Kinh doanh
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <h2>Tin tức</h2>
+        <ul>
+          <li>
+            <h4>
+              <a
+                href="#"
+                title="Trung Quốc điều thêm 17 tàu đến khu vực giàn khoan"
+              >
+                Trung Quốc điều thêm 17 tàu đến khu vực giàn khoan
+              </a>
+            </h4>
+            <a href="#" title="">
+              <img src="/images/h1.jpg" alt="h1.jpg" />
+            </a>
+            <p>
+              Để bảo vệ giàn khoan, Trung Quốc đã điều thêm 17 tàu các loại so với
+              hôm trước, sẵn sàng đâm va vào tàu Việt Nam
+            </p>
+          </li>
+          <li>
+            <h4>
+              <a href="#" title="Trọng tài - Vết đen của kỳ World Cup sôi động">
+                Trọng tài - Vết đen của kỳ World Cup sôi động
+              </a>
+            </h4>
+            <a href="#" title="">
+              <img src="/images/h2.jpg" alt="h2.jpg" />
+            </a>
+            <p>
+              World Cup 2014 chưa đi hết lượt đấu vòng bảng nhưng các trọng tài đẳng
+              cấp FIFA đã có tới 4 trận bị chỉ trích dữ dội
+            </p>
+          </li>
+          <li>
+            <h4>
+              <a href="#" title="Những mỹ nhân Việt duyên dáng ở tuổi tứ tuần">
+                Những mỹ nhân Việt duyên dáng ở tuổi tứ tuần
+              </a>
+            </h4>
+            <a href="#" title="">
+              <img src="/images/h4.jpg" alt="h4.jpg" />
+            </a>
+            <p>
+              Việt Trinh, Thu Hà, Hồng Nhung, Thanh Lam... vẫn giữ được nét thanh
+              xuân, tươi trẻ và cuốn hút theo thời gian nhờ phong cách làm đẹp tinh
+              tế.
+            </p>
+          </li>
+          <li>
+            <h4>
+              <a
+                href="#"
+                title="Chuyển nhượng 17/6: Arsenal mua Balotelli, tráo hàng Man Utd"
+              >
+                Chuyển nhượng 17/6: Arsenal mua Balotelli, tráo hàng Man Utd
+              </a>
+            </h4>
+            <a href="#" title="">
+              <img src="/images/h5.jpg" alt="h5.jpg" />
+            </a>
+            <p>
+              Đội bóng thành London đang đẩy mạnh việc tuyển những vị trí còn yếu
+              trong đội hình ở cả 3 tuyến
+            </p>
+          </li>
+          <li>
+            <h4>
+              <a href="#" title="Chuyên gia Anh tin chắc vị trí MH370 rơi">
+                Chuyên gia Anh tin chắc vị trí MH370 rơi
+              </a>
+            </h4>
+            <a href="#" title="">
+              <img src="/images/h1.jpg" alt="h1.jpg" />
+            </a>
+            <p>
+              Các chuyên gia thuộc công ty viễn thông Anh Inarat cho rằng họ xác
+              định được vị trí chiếc máy bay MH370 đã rơi xuống
+            </p>
+          </li>
+        </ul>
+      </div>
+      <a href="#" title="Trang 1">
+        Trang 1
+      </a>
+      |
+      <a href="#" title="Trang 2">
+        Trang 2
+      </a>
+      |
+      <a href="#" title="Trang 3">
+        Trang 3
+      </a>
+      |
+      <a href="#" title="Trang 4">
+        Trang 4
+      </a>
     </div>
   )
 }
